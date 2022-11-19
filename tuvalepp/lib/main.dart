0f3b33253ecf69da6c936b9f98cb001b2e7e4f42
Fazliç <fazlic407@gmail.com>
2022-11-19 22:52:40 +0300

    Made necessary configurations for googlemapsflutter package Created mapview.dart and added a fixed location and a custom marker for the map

--- tuvalepp/lib/main.dart
+++ tuvalepp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tuvalepp/pages/listview.dart';
+import 'package:tuvalepp/pages/mapview.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
           fontFamily: "Inter"),
-      home: const ListViewPage(),
+      home: const MapViewPage(),
     );
   }
 }
---
+++ tuvalepp/lib/pages/mapview.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class MapViewPage extends StatefulWidget {
+  const MapViewPage({super.key});
+
+  @override
+  State<MapViewPage> createState() => MapViewPageState();
+}
+
+class MapViewPageState extends State<MapViewPage> {
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
+  @override
+  void initState() {
+    addCustomIcon();
+    super.initState();
+  }
+
+  void addCustomIcon() {
+    BitmapDescriptor.fromAssetImage(
+            const ImageConfiguration(), "assets/poop_marker.png")
+        .then(
+      (icon) {
+        setState(() {
+          markerIcon = icon;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(41.045135, 29.034566),
+          zoom: 14,
+        ),
+        markers: {
+          Marker(markerId: MarkerId("poop"),
+          position: LatLng(41.045135, 29.034566),
+          draggable: false,
+          icon: markerIcon,
+          )
+        },
+      ),
+    );
+  }
+}
