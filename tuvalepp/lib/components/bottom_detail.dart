19854c248097684b6df9d072b9772610cdd590d2
Fazliç <fazlic407@gmail.com>
2022-11-22 23:37:21 +0300

    Changed buttomdetail to a component.

---
+++ tuvalepp/lib/components/bottom_detail.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+
+class BottomDetail extends StatefulWidget {
+  const BottomDetail({super.key});
+
+  @override
+  State<BottomDetail> createState() => _BottomDetailState();
+}
+
+// BUNA PARAMETRE KOYMAK LAZIM
+// DATABASEDEN MARKER BİLGİSİNİ ÇEKİP BUNA VERECEK
+
+class _BottomDetailState extends State<BottomDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Row(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(8.0),
+                          child: const Image(
+                              image: NetworkImage(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              fit: BoxFit.cover),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 20),
+                          child: Column(
+                            crossAxisAlignment:
+                                CrossAxisAlignment.start,
+                            mainAxisAlignment:
+                                MainAxisAlignment
+                                    .spaceBetween,
+                            children: [
+                              Text(
+                                "Title",
+                                style: TextStyle(
+                                    fontFamily: "Inter",
+                                    fontSize: 24,
+                                    fontWeight:
+                                        FontWeight.w600,
+                                    color: Colors.black),
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.man,
+                                    size: 30,
+                                    color: Colors.black,
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius
+                                                .circular(5),
+                                        color: Colors.pink),
+                                    child: const Padding(
+                                      padding: EdgeInsets
+                                          .symmetric(
+                                              vertical: 5,
+                                              horizontal: 10),
+                                      child: Text(
+                                        "★ 4.2",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.directions_walk,
+                                    size: 20,
+                                  ),
+                                  const Text(
+                                    "12 dk",
+                                    style: TextStyle(
+                                        fontWeight:
+                                            FontWeight.w500),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 80,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(8.0),
+                          child: const Image(
+                              image: NetworkImage(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              height: double.infinity,
+                              fit: BoxFit.cover),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(8.0),
+                          child: const Image(
+                              image: NetworkImage(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              height: double.infinity,
+                              fit: BoxFit.cover),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(8.0),
+                          child: const Image(
+                              image: NetworkImage(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              height: double.infinity,
+                              fit: BoxFit.cover),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(8.0),
+                          child: const Image(
+                              image: NetworkImage(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              height: double.infinity,
+                              fit: BoxFit.cover),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 75,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, "/detail");
+                      },
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<
+                            RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        backgroundColor:
+                            const MaterialStatePropertyAll<
+                                Color>(Colors.pink),
+                      ),
+                      child: const Text(
+                        "Detaya Git",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
--- tuvalepp/lib/pages/mapview.dart
+++ tuvalepp/lib/pages/mapview.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:tuvalepp/components/tile_card.dart';
 import 'package:tuvalepp/components/view_switch_button.dart';
+import 'package:tuvalepp/components/bottom_detail.dart';
 
 class MapViewPage extends StatefulWidget {
   const MapViewPage({super.key});
@@ -41,8 +42,8 @@ class MapViewPageState extends State<MapViewPage> {
             zoom: 14,
           ),
           markers: {
-            Marker(
-                markerId: MarkerId("poop"),
+            Marker( // BURAYA DATABASETEN MARKER BİLGİLERİ ÇEKİLİP MARKER 
+                markerId: MarkerId("poop"), // WİDGETLARINI ÇEVİRİP VERMEK LAZIM
                 position: LatLng(41.045135, 29.034566),
                 draggable: false,
                 icon: markerIcon,
@@ -50,193 +51,7 @@ class MapViewPageState extends State<MapViewPage> {
                   showModalBottomSheet(
                       context: context,
                       builder: (BuildContext context) {
-                        return Container(
-                          color: Colors.white,
-                          child: Container(
-                              child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: Column(
-                                  children: [
-                                    SizedBox(
-                                      height: 100,
-                                      child: Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8.0),
-                                            child: const Image(
-                                                image: NetworkImage(
-                                                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                                                fit: BoxFit.cover),
-                                          ),
-                                          Padding(
-                                            padding:
-                                                const EdgeInsets.only(left: 20),
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment
-                                                      .spaceBetween,
-                                              children: [
-                                                Text(
-                                                  "Title",
-                                                  style: TextStyle(
-                                                      fontFamily: "Inter",
-                                                      fontSize: 24,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                      color: Colors.black),
-                                                ),
-                                                Row(
-                                                  children: [
-                                                    const Icon(
-                                                      Icons.man,
-                                                      size: 30,
-                                                      color: Colors.black,
-                                                    ),
-                                                    const SizedBox(width: 10),
-                                                    Container(
-                                                      decoration: BoxDecoration(
-                                                          borderRadius:
-                                                              BorderRadius
-                                                                  .circular(5),
-                                                          color: Colors.pink),
-                                                      child: const Padding(
-                                                        padding: EdgeInsets
-                                                            .symmetric(
-                                                                vertical: 5,
-                                                                horizontal: 10),
-                                                        child: Text(
-                                                          "★ 4.2",
-                                                          style: TextStyle(
-                                                            color: Colors.white,
-                                                          ),
-                                                        ),
-                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                                Row(
-                                                  children: [
-                                                    const Icon(
-                                                      Icons.directions_walk,
-                                                      size: 20,
-                                                    ),
-                                                    const Text(
-                                                      "12 dk",
-                                                      style: TextStyle(
-                                                          fontWeight:
-                                                              FontWeight.w500),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 20,
-                                    ),
-                                    SizedBox(
-                                      height: 80,
-                                      child: ListView(
-                                        scrollDirection: Axis.horizontal,
-                                        children: <Widget>[
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8.0),
-                                            child: const Image(
-                                                image: NetworkImage(
-                                                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                                                height: double.infinity,
-                                                fit: BoxFit.cover),
-                                          ),
-                                          SizedBox(
-                                            width: 20,
-                                          ),
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8.0),
-                                            child: const Image(
-                                                image: NetworkImage(
-                                                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                                                height: double.infinity,
-                                                fit: BoxFit.cover),
-                                          ),
-                                          SizedBox(
-                                            width: 20,
-                                          ),
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8.0),
-                                            child: const Image(
-                                                image: NetworkImage(
-                                                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                                                height: double.infinity,
-                                                fit: BoxFit.cover),
-                                          ),
-                                          SizedBox(
-                                            width: 20,
-                                          ),
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(8.0),
-                                            child: const Image(
-                                                image: NetworkImage(
-                                                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                                                height: double.infinity,
-                                                fit: BoxFit.cover),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    SizedBox(
-                                      width: double.infinity,
-                                      height: 75,
-                                      child: TextButton(
-                                        onPressed: () {
-                                          Navigator.pushNamed(
-                                              context, "/detail");
-                                        },
-                                        style: ButtonStyle(
-                                          shape: MaterialStateProperty.all<
-                                              RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(8.0),
-                                            ),
-                                          ),
-                                          backgroundColor:
-                                              const MaterialStatePropertyAll<
-                                                  Color>(Colors.pink),
-                                        ),
-                                        child: const Text(
-                                          "Detaya Git",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.w700,
-                                              fontSize: 24),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          )),
-                        );
+                        return BottomDetail();
                       });
                 })
           },
